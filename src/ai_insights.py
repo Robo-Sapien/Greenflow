4aefe6e967573b011f1d338506c0b94f416d3f79
Robo-Sapien <shivambhagat314@gmail.com>
2025-09-21 15:48:08 +0530

    Updated api_version for GPT

--- src/ai_insights.py
+++ src/ai_insights.py
@@ -3,7 +3,7 @@ from openai import AzureOpenAI
 
 client = AzureOpenAI(
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2024-10-01-preview",
+    api_version="2024-12-01-preview",
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
 )
 
